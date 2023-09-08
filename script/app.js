c776e37f34e7fddc8305d7e89d9d112af6362152
Germar Bunda <123885878+Germar22@users.noreply.github.com>
2023-09-09 01:01:38 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -13,7 +13,7 @@ function displaydif() {
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum - secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
+  document.getElementById("answer2").innerHTML = ` ${firstNum} - ${secondNum}, equals to ${total}` ;
 }
 
 document.getElementById('difButton').addEventListener("click", displaydif);
